9580bc2433a0a12d5abefdc74e63eb8cb34e2f4d
Ron Sedee <roncd@hotmail.com>
2023-10-02 15:00:24 +0200

    blog ajax

--- script/script.js
+++ script/script.js
@@ -1,3 +1,22 @@
 $(() => {
-    $("#rechts").load("artikelen.html")
-})
+    $(document).on("ajaxError", (event, jqxhr) => {
+        alert("Er is een fout opgetreden:" + " " + jqxhr.status + " " + jqxhr.statusText);
+    });
+    function overzicht() {
+        $("#rechts").load("artikelen.html article:nth-of-type(-n+3)");
+    };
+    overzicht();
+    $(".zwart").on("click", () => {
+        $("#rechts").load("blogbeheer.html #rechts");
+    });
+    $(document).on("click", "article", (event) => {
+        $("article").not(event.currentTarget).remove();
+        $(event.currentTarget).css("font-size", "larger");
+        $(event.currentTarget).on("click", () => {
+            overzicht();
+        });
+    });
+    $(document).on("click", "button", () => {
+        overzicht();
+    });
+});
